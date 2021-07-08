fc8c0c0351220f025d4c29e15ad5336bbad78404
khloud44 <khloudelsaid44@gmail.com>
2021-07-08 10:29:53 +0200

    Search Section

--- js/main.js
+++ js/main.js
@@ -208,3 +208,64 @@ $(function () {
 ==========================================================================================================================================
 ==========================================================================================================================================
 */
+
+// -----------------search Section------------------------------------//
+
+$("#SearchDiv #CloseIcon").click(function () {
+  $("#SearchDiv").fadeOut(1000);
+});
+
+$(".get_blood").click(function () {
+  $("#SearchDiv").fadeIn(1000);
+});
+
+let BloodType=document.getElementById('SearchBloodType'),
+ City=document.getElementById('SearchCity'),
+ BtnSearch=document.querySelector('.saerchButton'),
+ TableBody=document.querySelector('.TableBody'),
+ result=document.querySelector('.result'),
+ table=document.querySelector('.table'),
+ SearchSection=document.getElementById('SearchDiv');
+
+
+
+ let SearchArray = [];
+ if ( localStorage.getItem("getBlood") == null){
+    SearchArray  = [];
+   }
+   else{
+    SearchArray  = JSON.parse(localStorage.getItem("getBlood"));
+   }
+
+
+
+
+ BtnSearch.addEventListener('click',function(){                 //Search Function
+  
+        let Trs='';
+        for(let i=0;i<SearchArray .length;i++){
+            if(SearchArray [i].loca==City.value 
+            && SearchArray [i].getBlood==BloodType.value){
+              table.classList.remove('d-none');           
+              
+                            Trs +=` <tr>                                                  
+                                    <td>${SearchArray [i].firstName} ${SearchArray [i].lastName}</td>
+                                    <td>${SearchArray [i].email}</td>
+                                    <td>${SearchArray [i].mobile}</td>
+                                </tr>` 
+                            TableBody.innerHTML=Trs; 
+                            result.innerHTML="" ; 
+              } else{
+                
+                // TableBody.innerHTML='';
+                table.classList.add('d-none');
+                result.innerHTML="Sorry, It's Not Avilable" ;  
+                }
+              
+               
+            }
+            BloodType.value='';
+            City.value='';
+
+            });
+
